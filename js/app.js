2f82ebc9f8e5c44fcc1f9c52d3dc335b07ff23bd
unknown <lampasonafrancois@gmail.com>
2023-09-26 23:28:33 +0200

    Versione 0.0.3.5-carosello-prova

--- js/app.js
+++ js/app.js
@@ -1,16 +1,14 @@
 new Swiper("#swiper-2", {
     slidesPerView: 1,
     centeredSlides: true,
+    spaceBetween: 20,
     pagination: {
-        el: ("#swiper-2 .swiper-custom-pagination"),
-        clickable: true,
+        el: ".swiper-custom-pagination", // Selettore dell'elemento in cui verranno creati i pallini
+        clickable: true, // Consente di fare clic sui pallini per navigare tra le slide
         renderBullet: function (index, className) {
-        return `<div class=${className}>
-            <span class="number">${index + 1}</span>
-            <span class="line"></span>
-            </div>`;
+          return `<span class="${className}"></span>`; // Creazione dei pallini
         }
-    },
+      },
     lazyLoading: true,
     loop: true,
     keyboard: {
@@ -22,47 +20,22 @@ new Swiper("#swiper-2", {
     },
     breakpoints: {
         200: {
-            slidesPerView: 1.2
+            slidesPerView: 1.3
         },
         400: {
             slidesPerView: 1.3
         },
         450: {
-            slidesPerView: 1.4
+            slidesPerView: 1.3
         },
-        500: {
-            slidesPerView: 1.5
-        },
-        550: {
-            slidesPerView: 1.6
-        },
-        600: {
-            slidesPerView: 1.7
-        },
-        650: {
-            slidesPerView: 1.8
-        },
-        700: {
-            slidesPerView: 2
-        },
-        800: {
-            slidesPerView: 2
-        },
-        900: {
-            slidesPerView: 2.2
+        768:{
+            slidesPerView: 1.9
         },
         950: {
-            slidesPerView: 1,
-            spaceBetween: 20,
+            slidesPerView: 2.3,
         },
-        1000: {
-            slidesPerView: 1.5,
-            spaceBetween: 5000
-
-        },
-        1400: {
-            slidesPerView: 3,
-            spaceBetween: 150
+        1200: {
+            slidesPerView: 2.7,
         }
     }
 });
