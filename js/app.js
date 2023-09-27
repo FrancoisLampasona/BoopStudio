69723dc7b72c55b9bacc5ae3682515241ca4eef4
unknown <lampasonafrancois@gmail.com>
2023-09-27 17:54:11 +0200

    Versione 0.0.3.6

--- js/app.js
+++ js/app.js
@@ -1,41 +1,61 @@
-new Swiper("#swiper-2", {
-    slidesPerView: 1,
-    centeredSlides: true,
-    spaceBetween: 20,
-    pagination: {
-        el: ".swiper-custom-pagination", // Selettore dell'elemento in cui verranno creati i pallini
-        clickable: true, // Consente di fare clic sui pallini per navigare tra le slide
-        renderBullet: function (index, className) {
-          return `<span class="${className}"></span>`; // Creazione dei pallini
-        }
-      },
-    lazyLoading: true,
-    loop: true,
-    keyboard: {
-        enabled: true,
+const swiper = new Swiper("#swiper-2", {
+  slidesPerView: 1.5, // Imposta il numero iniziale di slide visibili
+  centeredSlides: true,
+  spaceBetween: 20,
+  pagination: {
+    el: ".swiper-custom-pagination",
+    clickable: true,
+    renderBullet: function (index, className) {
+      return `<span class="${className}"></span>`;
     },
-    navigation: {
-        nextEl: "#nav-right",
-        prevEl: "#nav-left"
+  },
+  lazyLoading: true,
+  loop: true,
+  keyboard: {
+    enabled: true,
+  },
+  navigation: {
+    nextEl: "#nav-right",
+    prevEl: "#nav-left",
+  },
+  breakpoints: {
+    200: {
+      slidesPerView: 1.3,
     },
-    breakpoints: {
-        200: {
-            slidesPerView: 1.3
-        },
-        400: {
-            slidesPerView: 1.3
-        },
-        450: {
-            slidesPerView: 1.3
-        },
-        768:{
-            slidesPerView: 1.9
-        },
-        950: {
-            slidesPerView: 2.3,
-        },
-        1200: {
-            slidesPerView: 2.7,
-        }
-    }
+    400: {
+      slidesPerView: 1.3,
+    },
+    450: {
+      slidesPerView: 1.3,
+    },
+    768: {
+      slidesPerView: 1.9,
+    },
+    950: {
+      slidesPerView: 2.3,
+    },
+    1200: {
+      slidesPerView: 3.5,
+    },
+  },
+});
+
+const swiperContainer = document.querySelector("#swiper-2");
+
+swiperContainer.addEventListener("mouseover", () => {
+  swiper.params.slidesPerView = 1;
+  swiper.update();
+});
+
+swiperContainer.addEventListener("mouseout", () => {
+  swiper.params.slidesPerView = swiper.originalParams.slidesPerView; // Imposta il numero iniziale
+  swiper.update();
+});
+
+const swiperSlides = document.querySelectorAll(".swiper-slide");
+
+swiperSlides.forEach((slide) => {
+  slide.addEventListener("mouseover", () => {
+    slide.style.height = "10vw";
+  });
 });
