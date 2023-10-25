51ef677ea8dde9ac19101467c0f0a797cd2a0bcc
unknown <lampasonafrancois@gmail.com>
2023-10-25 17:14:38 +0200

    Versione 0.1.1 Prova Flip-Card in JS

--- js/flip-card.js
+++ js/flip-card.js
@@ -1,25 +1,30 @@
-const card = document.querySelector(".interno-card");
+const cards = document.querySelectorAll(".interno-card");
 const scopri = document.querySelector("#flip-link-button");
+const body = document.body;
 
-card.addEventListener("click", function () {
-  if (card.classList.contains("is-flipped")) {
-    // Se la card è stata girata e il link "Scopri di più" è stato cliccato
-
-      const linkScopriDiPiu = document.querySelector("#logo-text-scopri a");
+cards.forEach((card) => {
+  card.addEventListener("click", function () {
+    if (card.classList.contains("is-flipped")) {
+      // Se la card è stata girata e il link "Scopri di più" è stato cliccato
+      const linkScopriDiPiu = card.querySelector("#logo-text-scopri a");
       linkScopriDiPiu.click();
+    } else {
+      // Altrimenti, esegui il flip della card
+      card.classList.toggle("is-flipped");
+    }
+  });
 
-  } else {
-    // Altrimenti, esegui il flip della card
-    card.classList.toggle("is-flipped");
+  card.addEventListener("mouseleave", function () {
+    card.classList.remove("is-flipped");
+  });
+});
+
+body.addEventListener("click", function (e) {
+  // Controlla se il clic è avvenuto al di fuori di tutte le card
+  const isOutsideAllCards = Array.from(cards).every((card) => !card.contains(e.target));
+  if (isOutsideAllCards) {
+    cards.forEach((card) => {
+      card.classList.remove("is-flipped");
+    });
   }
-});
-
-card.addEventListener("mouseleave", function () {
-  card.classList.remove("is-flipped");
-});
-
-scopri.addEventListener("click", function () {
-  const linkScopriDiPiu = document.querySelector("#logo-text-scopri a");
-  linkScopriDiPiu.click();
-  scopri.classList.add("click");
 });
